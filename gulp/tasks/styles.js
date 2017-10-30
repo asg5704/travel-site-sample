f72edee3ddd808e5e39a68da899239e743e6a2e2
Alexander <alexander.schmidt.garcia@gmail.com>
2017-10-30 09:32:54 -0400

    update files

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp'),
+      postcss = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      cssvars = require('postcss-simple-vars'),
+      nested = require('postcss-nested'),
+      cssimport = require('postcss-import'),
+      mixins = require('postcss-mixins');
+
+gulp.task('styles', function() {
+  //Implement PostCSS
+  //Gulp.src - where to get from Asynchronous
+  return gulp.src('./app/assets/css/styles.css')
+    .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(err) {
+      console.log(err.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles/'));
+});
