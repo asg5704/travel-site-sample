47b78afaf22713f6734fc9f7e6e6ad02d0ce27df
Alexander <alexander.schmidt.garcia@gmail.com>
2017-11-06 14:53:11 -0500

    update header & nav sections

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,13 +4,14 @@ const gulp = require('gulp'),
       cssvars = require('postcss-simple-vars'),
       nested = require('postcss-nested'),
       cssimport = require('postcss-import'),
+      hexrgba = require('postcss-hexrgba'),
       mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
   //Implement PostCSS
   //Gulp.src - where to get from Asynchronous
   return gulp.src('./app/assets/css/styles.css')
-    .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]))
     .on('error', function(err) {
       console.log(err.toString());
       this.emit('end');
