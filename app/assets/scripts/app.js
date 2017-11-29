de654ff79fc96f8a23429a3d476a5fa841c12ed4
Alexander <alexander.schmidt.garcia@gmail.com>
2017-11-28 21:36:19 -0500

    complete mobile and desktop header styles

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,21 @@
+//Node version to use require
+//var Person = require('./modules/Person');
+
+//ES6 Importing
+// import Person from './modules/Person';
+
+// class Adult extends Person {
+//   payTaxes() {
+//     console.log(`${this.name} you owe me money`)
+//   }
+// }
+// var john = new Person('John', 'blue');
+// john.greet();
+
+// var jane = new Adult('Jane', 'purple');
+// jane.greet();
+// jane.payTaxes();
+
+import MobileMenu from './modules/MobileMenu';
+
+const mobileMenu = new MobileMenu();
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  entry: "./app/assets/scripts/app.js",
+  output: {
+    path: path.resolve(__dirname, "./app/temp/scripts/"),
+    filename: "App.js"
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
